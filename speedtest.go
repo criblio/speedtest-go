a2e44c63f625b0c616ff08dd0d6ac64fdfa0fdcc
Clint Sharp <clint@cribl.io>
2021-07-05 17:39:01 -0700

    setting time format to RFC3339

--- speedtest.go
+++ speedtest.go
@@ -189,6 +189,6 @@ func checkError(err error) {
 }
 
 func (t outputTime) MarshalJSON() ([]byte, error) {
-	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("2006-01-02 15:04:05.000"))
+	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format(time.RFC3339))
 	return []byte(stamp), nil
 }
